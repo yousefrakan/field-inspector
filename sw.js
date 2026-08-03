92a044ef7eacaf857b034e72b0fad605dd554239
yousefrakan <yousefrakanpre@gmail.com>
2026-08-03 18:14:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'field-inspector-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e)=>{
+  // network-first for navigation, cache fallback
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(()=> caches.match(e.request).then(r=> r || caches.match('./index.html')))
+  );
+});
